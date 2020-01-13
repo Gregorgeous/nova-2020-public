d1c85c643c111b64672774269d7bf2a8f600a40d
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2020-01-13 23:28:02 +0100

    FIXING: trying to have a PL locale in NetlifyCMS admin

--- src/netlifyCMS/cms.js
+++ src/netlifyCMS/cms.js
@@ -2,5 +2,8 @@ import CMS from 'netlify-cms-app'
 import { pl } from 'netlify-cms-locales'
 
 CMS.registerLocale('pl', pl)
+console.log('TEST')
+const localePL = CMS.getLocale('pl')
+console.log(localePL)
 
-CMS.init()
+CMS.init({ locale: localePL })
