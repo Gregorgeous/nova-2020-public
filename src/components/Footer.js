54e053c8435865691956a586c38fb382dddcccb9
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2020-02-08 18:46:13 +0000

    Fill in the footer section.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,45 @@
 import React from 'react'
 
-const Footer = (props) => (
-    <footer id="footer">
-        <div className="inner">
-            <ul className="icons">
-                <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
-            </ul>
-            <ul className="copyright">
-                <li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
-            </ul>
-        </div>
-    </footer>
+const Footer = props => (
+  <footer className="footer-inner" id="footer">
+    {/* <ul className="icons">
+        <li>
+          <a href="#" className="icon alt fa-twitter">
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon alt fa-facebook">
+            <span className="label">Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon alt fa-instagram">
+            <span className="label">Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon alt fa-github">
+            <span className="label">GitHub</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon alt fa-linkedin">
+            <span className="label">LinkedIn</span>
+          </a>
+        </li>
+      </ul> */}
+    <ul className="copyright">
+      <li>Hufiec Warszawa-Żoliborz &copy; 2020</li>
+      <li>
+        Motyw graficzny: <a href="https://html5up.net">HTML5 UP</a>
+      </li>
+      <li>
+        Wykonanie:{' '}
+        <a href="https://www.linkedin.com/in/grzegorz-rybak/">Grzegorz Rybak</a>
+      </li>
+    </ul>
+  </footer>
 )
 
 export default Footer
